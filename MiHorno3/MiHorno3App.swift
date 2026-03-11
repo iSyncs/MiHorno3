b7ece0a58ab7faa3f931a98298fdbafe3106bfa6
iSyncs <149739497+iSyncs@users.noreply.github.com>
2026-03-11 16:30:54 -0600

    Initial Commit

---
+++ MiHorno3/MiHorno3App.swift
@@ -0,0 +1,17 @@
+//
+//  MiHorno3App.swift
+//  MiHorno3
+//
+//  Created by Carlos Baranda on 11/03/26.
+//
+
+import SwiftUI
+
+@main
+struct MiHorno3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MiHorno3Tests/MiHorno3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  MiHorno3Tests.swift
+//  MiHorno3Tests
+//
+//  Created by Carlos Baranda on 11/03/26.
+//
+
+import Testing
+@testable import MiHorno3
+
+struct MiHorno3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
